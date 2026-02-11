cde7087e63dee5156ea76dd2b7f1d2da9f083862
aboubakertounli <aboubakertounli@gmail.com>
2026-02-11 22:19:05 +0100

    groupAnagrams

---
+++ Code3.py
@@ -0,0 +1,8 @@
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    groups = {}
+    for word in strs:
+        key = "".join(sorted(word))
+        if key not in groups:
+            groups[key] = []
+        groups[key].append(word)
+    return list(groups.values())
