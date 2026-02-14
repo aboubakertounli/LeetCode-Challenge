dc622f040c335430f8a50251ea73af3ed93ed0e3
aboubakertounli <aboubakertounli@gmail.com>
2026-02-14 10:45:24 +0100

    test code5

--- Code5.py
+++ Code5.py
@@ -1,22 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        r=[]
-        l=list(s)
-        for i in range(len(s)) :
-                m=l.pop(i)
-                if m in l:
-                    r.append(m)
-        print(r)
-        return len("".join(r))
-# Example usage:
-solution = Solution()
+        seen = set()
+        left = 0
+        max_length = 0
 
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            
+            seen.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+# Example usage:
+
+solution = Solution()
 s = "abcabcbb"
 result = solution.lengthOfLongestSubstring(s)
-print(result)  # Output: 3
+print(result)  # Output: 3 (the longest substring is "abc")
 s = "bbbbb"
 result = solution.lengthOfLongestSubstring(s)
-print(result)  # Output: 1
+print(result)  # Output: 1 (the longest substring is "b")
 s = "pwwkew"
 result = solution.lengthOfLongestSubstring(s)
-print(result)  # Output: 3
+print(result)  # Output: 3 (the longest substring is "wke")
