dc4d8dfefb535ea176b4ab025a8df148686bcb2e
aboubakertounli <aboubakertounli@gmail.com>
2026-02-17 16:51:42 +0000

    Fix Merge function

--- Merge-Intervals.py
+++ Merge-Intervals.py
@@ -4,9 +4,10 @@ class Solution:
         print(m)
         i=0
         while i<len(m)-1:
-            if m[i][1] >= m[i+1][0] :
-                m[i][1]=m[i+1][1]
+            if  m[i][1] >= m[i+1][1]:
                 m.remove(m[i+1])
+            elif m[i][1] >= m[i+1][0] :
+                m[i][1]=m[i+1][1]
             else:
                 i+=1
         return m
