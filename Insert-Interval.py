3c26fcc9d57e647e70268f581c9bc44133732ec9
aboubakertounli <aboubakertounli@gmail.com>
2026-02-20 22:59:11 +0000

    Insert Interval last commit

--- Insert-Interval.py
+++ Insert-Interval.py
@@ -13,12 +13,3 @@ class Solution:
             else:
                 i+=1
         return m
-example=Solution()
-print(example.insert([[1,3],[6,9]],[2,5]))
-# output: [[1,5],[6,9]]
-print(example.insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
-# output: [[1,2],[3,10],[12,16]]
-print(example.insert([[1,5]],[2,3]))
-# output: [[1,5]]
-print(example.insert([[1,5]],[2,7]))
-# output: [[1,7]]
